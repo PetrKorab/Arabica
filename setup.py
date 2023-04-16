6126af3b5bed21178281c0178804edd2fc5e3a75
Petr Koráb <xpetrkorab@gmail.com>
2023-04-17 01:26:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
         name="arabica",
-        version="1.0.2",
+        version="1.4.3",
         author="Petr Koráb",
         author_email="xpetrkorab@gmail.com",
         packages=["arabica"],

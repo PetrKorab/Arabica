b19259b481d96b460d45d5987e16232666534305
Petr Koráb <xpetrkorab@gmail.com>
2023-08-24 23:14:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Petr Korab'
 author = 'Petr Korab'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.9'
+release = '1.7.1'
 
 
 # -- General configuration ---------------------------------------------------
